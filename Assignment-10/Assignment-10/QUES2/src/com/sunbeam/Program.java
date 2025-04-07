5a774307ac9b7ec86a07d13ce5bdc5e46e86058f
saransh <saranshvats041@gmail.com>
2025-04-07 22:17:21 +0530

    Assignment-10

---
+++ Assignment-10/Assignment-10/QUES2/src/com/sunbeam/Program.java
@@ -0,0 +1,22 @@
+package com.sunbeam;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Program {
+	
+	public static int add(int a , int b) {
+		return IntStream.range(a, b).sum();
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the two number of factiorials :");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println("The Addition value is : "+add(num1,num2));
+
+	}
+
+}
